05ed462cbe412aee309a49432f8e5e7b02c8c5b8
Aum Zaveri <aumzaveri06@gmail.com>
2024-09-23 10:40:45 -0400

    CRUD operations added in controller with service layer

---
+++ microservice-parent/product-service/src/main/java/ca/gbc/productservice/controller/ProductController.java
@@ -0,0 +1,53 @@
+package ca.gbc.productservice.controller;
+
+import ca.gbc.productservice.dto.ProductRequest;
+import ca.gbc.productservice.dto.ProductResponse;
+import ca.gbc.productservice.model.Product;
+import ca.gbc.productservice.service.ProductService;
+import io.micrometer.core.ipc.http.HttpSender;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest){
+        productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts(){
+        return productService.getAllProducts();
+    }
+
+    @PutMapping
+    //@ResponseStatus(HttpStatus.NO_CONTENT )
+    //http:localhost:8079/api/product/jlqdfk
+    public ResponseEntity<?> updateProduct(@PathVariable("productId") String productId,
+                                           @RequestBody ProductRequest productRequest){
+        String updatedProductId = productService.updateProduct(productId, productRequest);
+
+        //set the location header attribute
+        HttpHeaders header = new HttpHeaders();
+        header.add("Location", "/api/product" + updatedProductId);
+        return new ResponseEntity<>(header, HttpStatus.NO_CONTENT);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> deleteProduct(@PathVariable("productId") String productId){
+        productService.deleteProduct(productId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+}
