0164060da162cc7acfb1b523854942d35e48b73d
Aum Zaveri <aumzaveri06@gmail.com>
2024-11-23 13:02:23 -0500

    final push for ICE 2

--- microservice-parent/api-gateway/src/main/java/ca/gbc/apigateway/routes/Routes.java
+++ microservice-parent/api-gateway/src/main/java/ca/gbc/apigateway/routes/Routes.java
@@ -89,18 +89,16 @@ public class Routes {
 
         return GatewayRouterFunctions.route("product_service_swagger")
                 .route(RequestPredicates.path("/aggregate/product-service/v3/api-docs"),
-                        HandlerFunctions.http("http://localhost:8084"))
+                        HandlerFunctions.http(productServiceUrl))
                 .filter(setPath("/api-docs"))
                 .build();
-
     }
 
     @Bean
     public RouterFunction<ServerResponse> orderServiceSwaggerRoute(){
-
         return GatewayRouterFunctions.route("order_service_swagger")
                 .route(RequestPredicates.path("/aggregate/order-service/v3/api-docs"),
-                        HandlerFunctions.http("http://localhost:8082"))
+                        HandlerFunctions.http(orderServiceUrl))
                 .filter(setPath("/api-docs"))
                 .build();
 
@@ -108,12 +106,10 @@ public class Routes {
 
     @Bean
     public RouterFunction<ServerResponse> inventoryServiceSwaggerRoute(){
-
         return GatewayRouterFunctions.route("inventory_service_swagger")
                 .route(RequestPredicates.path("/aggregate/inventory-service/v3/api-docs"),
-                        HandlerFunctions.http("http://localhost:8083"))
+                        HandlerFunctions.http(inventoryServiceUrl))
                 .filter(setPath("/api-docs"))
                 .build();
-
     }
 }
